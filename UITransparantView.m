da0ac3cbd2578463f021848e3a3ed7e819fc3b3d
Xuan <liuxuan30@users.noreply.github.com>
2015-09-01 11:36:45 +0800

    delete meaningless override

--- UITransparantView.m
+++ UITransparantView.m
@@ -24,11 +24,6 @@
 
 @implementation UITransparantView
 
-- (id)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
-    return self;
-}
-
 -(BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
     for (UIView *view in self.subviews) {
         if (!view.hidden && view.alpha > 0 && view.userInteractionEnabled && [view pointInside:[self convertPoint:point toView:view] withEvent:event])
